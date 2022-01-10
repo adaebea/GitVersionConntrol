894e6a63dc93a33f306672ab00f19eb5da406bf6
zhanghui <aigooe@163.com>
2022-01-10 10:13:48 +0800

    Initial Commit

---
+++ Shared/GitVersionConntrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitVersionConntrolApp.swift
+//  Shared
+//
+//  Created by 张辉 on 2022/1/10.
+//
+
+import SwiftUI
+
+@main
+struct GitVersionConntrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
